0e2e0e3f97d8d528603f564445b93585e69d638d
Santino Exequiel Pilot <santinoexequielpilot0@gmail.com>
2025-05-20 14:19:27 -0300

    Correccion del ejercicio 4

--- Ejercicio4/main.js
+++ Ejercicio4/main.js
@@ -7,6 +7,6 @@ boton.addEventListener("click",() => {
     const peso = parseFloat(document.getElementById("peso_usuario").value);
     const altura = parseFloat(document.getElementById("altura_usuario").value);
     let calculo = (peso / (altura**2)).toFixed(2);
-    IMC.innerHTML = `<p>Tu IMC es de <strong>${calculo}</strong></p>`;
+    alert(`Tu IMC es de ${calculo}`);
 })
 
